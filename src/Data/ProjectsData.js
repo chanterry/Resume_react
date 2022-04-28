bded5863ee05f675d7af8aa201d85a3495710c35
chanterry <95472500+terryaki75@users.noreply.github.com>
2022-04-28 15:57:29 +0200

    update

--- src/Data/ProjectsData.js
+++ src/Data/ProjectsData.js
@@ -1,13 +1,20 @@
 const DataProjects = [ 
     {
         id: 1,
+        title:'Projet Crypto App',
+        description:'Marché financier des crypto-monnaies',
+        link:'chanterry.github.io/crypto-app',
+        tools:'React, JavaScript',
+    },
+    {
+        id: 2,
         title:'Projet La Cité',
         description:'Refonte du site web d une association cultuelle',
         link:'fr.egliselacite.com',
         tools:'HTML, CSS, JavaScript',
     },
     {
-        id: 2,
+        id: 3,
         title:'Projet Bric & Broc',
         description:'Site de location de jouets par abonnement',
         link:'www.behance.net/gallery/135536637/Bric-Broc-Toy-rental-Website',
